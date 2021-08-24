34a2f77f5cdbfafaf2a594e8e63a84d4c0fb7dff
ShreyaKarmakar <44618459+ShreyaKarmakar@users.noreply.github.com>
2021-08-25 01:17:16 +0530

    Create MaxSumContiguousSubarray.java

---
+++ arrays/MaxSumContiguousSubarray.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    int max=Integer.MIN_VALUE;
+    int sum=0;
+    public int maxSubArray(final int[] A) {
+        for(int e:A){
+            sum += e;
+            if(sum>max){
+                max = sum;
+                
+            }
+            if(sum<0)
+                sum=0;
+        }
+        return max;
+    }
+}
